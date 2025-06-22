a61641714f42e027dae5fd0f937d080e7991275b
AngelT082505 <angeltaveras0825@gmail.com>
2025-06-22 18:47:09 +0200

    Second Commit

--- src/main/java/com/pm/login/config/WebConfig.java
+++ src/main/java/com/pm/login/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:4200")
+                        .allowedOriginPatterns("http://localhost:4200","https://gamevault8.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
